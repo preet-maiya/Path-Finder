90a48f4d28db92ff2fdc509e8425462ed0fc2d58
preet-maiya <mail.maiyapreetham@gmail.com>
2017-09-19 23:31:55 +0530

    Added RSSI value script

---
+++ bluetoothRSSI.py
@@ -0,0 +1,39 @@
+import sys,time,math
+import matplotlib.pyplot as plt
+from bt_proximity import BluetoothRSSI
+t = time.time()
+mini=100
+maxi=-100
+x=[]
+y=[]
+elapsed = 0.0
+t1 = 0.0
+dist = 0.0
+if len(sys.argv)<1:
+	print "usage python bluetoothRSSI <address>"
+else:
+    while 1:
+        try:
+            rssi = BluetoothRSSI(addr = sys.argv[1])
+            value = float(rssi.get_rssi())
+            if value != 0:
+                if mini>value:
+                    mini = value
+                if maxi<value:
+                    maxi = value
+		t1 = time.time()
+		elapsed = elapsed + (t1 - t)
+                print value," after time ",elapsed," seconds"
+                t = time.time()
+		dist = math.pow(10,-value) 
+		x.append(elapsed)
+		y.append(value)
+        except KeyboardInterrupt:
+            print "Minimum: ",mini," Maximum: ",maxi
+	    plt.plot(x,y, marker='o')
+	    plt.show()
+	    bins = range(int(min(y)),int(max(y)))
+	    plt.hist(y)
+	    plt.show()
+            sys.exit(0)
+ 
---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python
+sudo apt-get install bluetooth
+sudo apt-get install python-bluez
+sudo pip install bt_proximity
+sudo pip install 
